e090a36b3dd715f3cf6d222757ed443249ea2b50
Thiago Vasconcelos <tgoncalvesva2020@fau.edu>
2022-09-06 18:33:21 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Thiago Goncalves Vasconcelos on 9/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
